a3254ee993a4a730e460287572ba303b21fcaf60
jdspell <spelljd15@gmail.com>
2019-01-25 12:37:30 -0500

    completed functions.js assignment

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = function(arg1, arg2, cb){
+  return cb(arg1, arg2);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,15 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (num1, num2) => {return num1 + num2};
+const multiply = (num1, num2) => {return num1*num2};
+const greeting = (first_name, last_name) => {return `Hello ${first_name} ${last_name}, nice to meet you!`};
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +33,11 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+/*
+nestedfunction is a closure. Closures have access to their own scope, the scope of the function containing them and 
+the global scope as well. Since internal is within the scope of the myFunction, which contains nestedfunction then
+nested function has access to that variable.
+*/
 
 const external = "I'm outside the function";
 
